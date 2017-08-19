e648becda815073e7f9b58dea45edd0edc523f69
Konrad Sroka <cunraid@gmail.com>
2017-08-19 20:54:58 +1000

    customizer start >> admin bar option

--- includes/admin/customizer-options.php
+++ includes/admin/customizer-options.php
@@ -1,22 +1,40 @@
 <?php
 
-// Adding the Marketplace Pro theme options to the theme customizer
+// Adding Larry's theme options to the theme customizer
 
 function larry_customizer( $wp_customize ) {
+
 	$wp_customize->add_section(
 		'larry_section_one',
 		array(
-			'title' => 'Konrad Settings',
+			'title' => 'Larry Theme Options',
 			'description' => 'This is a settings section.',
 			'priority' => 35,
 		)
 	);
+
+
+	$wp_customize->add_setting( 'larry_show_admin_bar', array(
+	  'capability' => 'edit_theme_options',
+	  'sanitize_callback' => 'larry_sanitize_checkbox',
+	) );
+
+	$wp_customize->add_control( 'larry_show_admin_bar', array(
+	  'type' => 'checkbox',
+	  'section' => 'larry_section_one', // Add a default or your own section
+	  'label' => __( 'WP Admin Bar' ),
+	  'description' => __( 'Show WP Admin bar in front end?' ),
+	) );
+
+
+	// Text Box
 	$wp_customize->add_setting(
 		'konrad_textbox',
 		array(
 			'default' => 'Hallo Konrad',
 		)
 	);
+
 	$wp_customize->add_control(
 		'konrad_textbox',
 		array(
@@ -25,5 +43,7 @@ function larry_customizer( $wp_customize ) {
 			'type' => 'text',
 		)
 	);
+
+
 }
 add_action( 'customize_register', 'larry_customizer' );
