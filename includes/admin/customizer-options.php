76ad3dd5090fbecc94c73de3218b54ec6340bf69
Konrad Sroka <cunraid@gmail.com>
2017-08-19 22:23:04 +1000

    the perfect naming

--- includes/admin/customizer-options.php
+++ includes/admin/customizer-options.php
@@ -36,34 +36,34 @@ function larry_customizer( $wp_customize ) {
 
 	// Brand Color
 
-	$wp_customize->add_setting( 'larry_brand_color', array(
+	$wp_customize->add_setting( 'larry_link_color', array(
 	 'sanitize_callback' => 'sanitize_hex_color',
 	 'default'           => 'default',
 	 'transport'         => 'refresh',
 	 'default'						=> '#09b1cc'
 	) );
 
-	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'larry_brand_color', array(
-	 'label'    => __('Brand Color', 'larry'),
-	 'description' => __('Used for all your links. ', 'larry'),
+	$wp_customize->add_control( new WP_Customize_Color_Control( $wp_customize, 'link_color', array(
+	 'label'    => __('Link Color', 'larry'),
+	 'description' => __('Used for all your links. For example your brand color.', 'larry'),
 	 'section'  => 'colors',
-	 'settings' => 'larry_brand_color',
+	 'settings' => 'larry_link_color',
 	 'priority' => 20
 	) ) );
 
 
 	// Brand Logo
 
-	$wp_customize->add_setting( 'larry_brand_logo', array(
+	$wp_customize->add_setting( 'larry_site_logo', array(
 		'transport'		=> 'refresh',
 	) );
 
 	$wp_customize->add_control(
-     new WP_Customize_Image_Control( $wp_customize, 'larry_brand_logo', array(
-       'label'      	=> __( 'Brand Logo', 'larry' ),
-			 'description'  => __( 'Upload your brand logo. Used in top navigation.', 'larry' ),
+     new WP_Customize_Image_Control( $wp_customize, 'site_logo', array(
+       'label'      	=> __( 'Site Logo', 'larry' ),
+			 'description'  => __( 'Upload your site logo. Used in top navigation.', 'larry' ),
        'section'    	=> 'title_tagline',
-       'settings'   	=> 'larry_brand_logo',
+       'settings'   	=> 'larry_site_logo',
 			 'priority' 		=> 40
 	 ) ) );
 
