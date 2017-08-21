b83da2a633c96b3b3a7044b582b5924ee0a136fe
Konrad Sroka <cunraid@gmail.com>
2017-08-21 10:26:27 +1000

    fixes

--- archive-product.php
+++ archive-product.php
@@ -22,113 +22,117 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 get_header( 'shop' ); ?>
 
-	<?php
-		/**
-		 * woocommerce_before_main_content hook.
-		 *
-		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
-		 * @hooked woocommerce_breadcrumb - 20
-		 * @hooked WC_Structured_Data::generate_website_data() - 30
-		 */
-		do_action( 'woocommerce_before_main_content' );
-	?>
-
-    <header class="woocommerce-products-header">
-
-		<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
-
-			<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
-
-		<?php endif; ?>
-
-		<?php
-			/**
-			 * woocommerce_archive_description hook.
-			 *
-			 * @hooked woocommerce_taxonomy_archive_description - 10
-			 * @hooked woocommerce_product_archive_description - 10
-			 */
-			do_action( 'woocommerce_archive_description' );
-		?>
-
-    </header>
-
-		<?php if ( have_posts() ) : ?>
+<div class="main-content">
+	<div class="container">
+		<div class="row">
 
 			<?php
 				/**
-				 * woocommerce_before_shop_loop hook.
+				 * woocommerce_before_main_content hook.
 				 *
-				 * @hooked wc_print_notices - 10
-				 * @hooked woocommerce_result_count - 20
-				 * @hooked woocommerce_catalog_ordering - 30
+				 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+				 * @hooked woocommerce_breadcrumb - 20
+				 * @hooked WC_Structured_Data::generate_website_data() - 30
 				 */
-				do_action( 'woocommerce_before_shop_loop' );
+				do_action( 'woocommerce_before_main_content' );
 			?>
 
-			<?php woocommerce_product_loop_start(); ?>
+		    <header class="woocommerce-products-header">
 
-				<?php woocommerce_product_subcategories(); ?>
+				<?php if ( apply_filters( 'woocommerce_show_page_title', true ) ) : ?>
 
-				<?php while ( have_posts() ) : the_post(); ?>
+					<h1 class="woocommerce-products-header__title page-title"><?php woocommerce_page_title(); ?></h1>
+
+				<?php endif; ?>
+
+				<?php
+					/**
+					 * woocommerce_archive_description hook.
+					 *
+					 * @hooked woocommerce_taxonomy_archive_description - 10
+					 * @hooked woocommerce_product_archive_description - 10
+					 */
+					do_action( 'woocommerce_archive_description' );
+				?>
+
+		    </header>
+
+				<?php if ( have_posts() ) : ?>
 
 					<?php
 						/**
-						 * woocommerce_shop_loop hook.
+						 * woocommerce_before_shop_loop hook.
 						 *
-						 * @hooked WC_Structured_Data::generate_product_data() - 10
+						 * @hooked wc_print_notices - 10
+						 * @hooked woocommerce_result_count - 20
+						 * @hooked woocommerce_catalog_ordering - 30
 						 */
-						do_action( 'woocommerce_shop_loop' );
+						do_action( 'woocommerce_before_shop_loop' );
 					?>
 
-					<?php wc_get_template_part( 'content', 'product' ); ?>
+					<?php woocommerce_product_loop_start(); ?>
 
-				<?php endwhile; // end of the loop. ?>
+						<?php woocommerce_product_subcategories(); ?>
 
-			<?php woocommerce_product_loop_end(); ?>
+						<?php while ( have_posts() ) : the_post(); ?>
+
+							<?php
+								/**
+								 * woocommerce_shop_loop hook.
+								 *
+								 * @hooked WC_Structured_Data::generate_product_data() - 10
+								 */
+								do_action( 'woocommerce_shop_loop' );
+							?>
+
+							<?php wc_get_template_part( 'content', 'product' ); ?>
+
+						<?php endwhile; // end of the loop. ?>
+
+					<?php woocommerce_product_loop_end(); ?>
+
+					<?php
+						/**
+						 * woocommerce_after_shop_loop hook.
+						 *
+						 * @hooked woocommerce_pagination - 10
+						 */
+						do_action( 'woocommerce_after_shop_loop' );
+					?>
+
+				<?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
+
+					<?php
+						/**
+						 * woocommerce_no_products_found hook.
+						 *
+						 * @hooked wc_no_products_found - 10
+						 */
+						do_action( 'woocommerce_no_products_found' );
+					?>
+
+				<?php endif; ?>
 
 			<?php
 				/**
-				 * woocommerce_after_shop_loop hook.
+				 * woocommerce_after_main_content hook.
 				 *
-				 * @hooked woocommerce_pagination - 10
+				 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
 				 */
-				do_action( 'woocommerce_after_shop_loop' );
+				do_action( 'woocommerce_after_main_content' );
 			?>
 
-		<?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
-
-			<?php
+		  <?php
 				/**
-				 * woocommerce_no_products_found hook.
+				 * woocommerce_sidebar hook.
 				 *
-				 * @hooked wc_no_products_found - 10
+				 * @hooked woocommerce_get_sidebar - 10
 				 */
-				do_action( 'woocommerce_no_products_found' );
+				//do_action( 'woocommerce_sidebar' );
 			?>
 
+		<?php if ( get_theme_mod( 'larry_wc_archive_sidebars' ) == true ) : ?>
+				<?php get_sidebar( 'shop' ); ?>
 		<?php endif; ?>
-
-	<?php
-		/**
-		 * woocommerce_after_main_content hook.
-		 *
-		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
-		 */
-		do_action( 'woocommerce_after_main_content' );
-	?>
-
-  <?php
-		/**
-		 * woocommerce_sidebar hook.
-		 *
-		 * @hooked woocommerce_get_sidebar - 10
-		 */
-		//do_action( 'woocommerce_sidebar' );
-	?>
-
-<?php if ( get_theme_mod( 'larry_wc_archive_sidebars' ) == true ) : ?>
-		<?php get_sidebar( 'shop' ); ?>
-<?php endif; ?>
 
 <?php get_footer( 'shop' ); ?>
