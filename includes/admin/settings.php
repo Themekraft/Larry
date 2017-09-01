f5d9ba79bdd733e5cb935e529c2305c846b186da
Konrad Sroka <cunraid@gmail.com>
2017-09-01 23:36:43 +1000

    welcome page part 2

--- includes/admin/settings.php
+++ includes/admin/settings.php
@@ -30,7 +30,7 @@ function larry_screen() { ?>
 				<div class="larry-panel-column-container">
 					<div class="larry-col-small">
 						<h3 class="larry-subtitle">First Step</h3>
-						<p><a class="button button-primary button-larry" href="/wp-admin/customize.php" style=""><i class="dashicons dashicons-admin-appearance"></i> &nbsp; Customize Your Site</a></p>
+						<p><a class="button button-primary button-larry" href="<?php echo home_url(); ?>/wp-admin/customize.php" style=""><i class="dashicons dashicons-admin-appearance"></i> &nbsp; Customize Your Site</a></p>
 						<!-- <p class="hide-if-no-customize">or <a href="http://localhost:8888/testing/wp-admin/themes.php">change your theme completely</a></p> -->
 					</div>
 					<div class="larry-col-small">
@@ -47,10 +47,10 @@ function larry_screen() { ?>
 					</div>
 					<div class="larry-col-big">
 						<h3 class="larry-subtitle">Need Help?</h3>
-						<p><a class="button xbutton-primary button-larry" href="#" style=""><i class="dashicons dashicons-admin-page"></i> &nbsp; View Documentation</a>
+						<p><a class="button xbutton-primary button-larry" href="#" style=""><i class="dashicons dashicons-search"></i> &nbsp; View Documentation</a>
 						<!-- </p>
 						<p> -->
-							<a class="button xbutton-primary button-larry" href="#" style=""><i class="dashicons dashicons-email"></i> &nbsp; Ask Support</a></p>
+							<a class="button xbutton-primary button-larry" href="#" style=""><i class="dashicons dashicons-email-alt"></i> &nbsp; Ask Support</a></p>
 
 					</div>
 				</div>
@@ -65,21 +65,21 @@ function larry_screen() { ?>
 
 
 
-		<div class="larry-panel">
-			<div class="larry-panel-content">
+		<div class="xlarry-panel">
+			<div class="xlarry-panel-content">
 
-				<h3 class="larry-subtitle">See Demo Sites</h3>
+				<h3 class="xlarry-subtitle" style="margin: 50px 0 0;">Demo Sites</h3>
 				<div class="larry-panel-column-container">
 					<div class="larry-col-small">
 						<p class="larry-lead">A Simple WordPress Demo:</p>
 						<!-- <p>See a minimal demo - just a WordPress install</p> -->
-		        <p><a href="https://themekraft.com/larry-demo/" class="button button-primary button-larry" title="View Larry's Minimal Demo" target="_blank">View Simple Demo</a></p>
+		        <p><a href="https://themekraft.com/larry-demo/" class="button xbutton-primary button-larry" title="View Larry's Minimal Demo" target="_blank">View Simple Demo</a></p>
 					</div>
 
 					<div class="larry-col-big">
 		        <p class="larry-lead">WooCommerce &amp; BuddyPress Demo:</p>
 						<!-- <p>See the demo site with WooCommerce and BuddyPress</p> -->
-		        <p><a href="https://themekraft.com/larry-demo-full/" class="button button-primary button-larry" title="View Larry's Full Demo" target="_blank">View Full Demo</a></p>
+		        <p><a href="https://themekraft.com/larry-demo-full/" class="button xbutton-primary button-larry" title="View Larry's Full Demo" target="_blank">View Full Demo</a></p>
 					</div>
 				</div>
 
